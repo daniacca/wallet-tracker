7b9bf2ada01dde7db6deb72f0628e7e17db0d968
daniacca <d.acca87@gmail.com>
2023-10-31 14:20:36 +0100

    chore: update gitignore for ignoring build output directories

--- src/webapi/build/entity/transaction.js
+++
@@ -1,6 +0,0 @@
-export var TransactionType;
-(function (TransactionType) {
-    TransactionType["INCOME"] = "INCOME";
-    TransactionType["EXPENSE"] = "EXPENSE";
-})(TransactionType || (TransactionType = {}));
-//# sourceMappingURL=transaction.js.map
