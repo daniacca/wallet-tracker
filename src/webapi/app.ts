76a5139409e53c1b7a931b351f4a0135688769ae
daniacca <d.acca87@gmail.com>
2023-10-31 14:47:20 +0100

    infra: added DB connection, cors / json middleware

--- src/webapi/app.ts
+++ src/webapi/app.ts
@@ -1,17 +1,35 @@
 import express from "express";
 import dotenv from "dotenv";
 import findConfig from "find-config";
+import cors from "cors";
+import connectDB from "./config/db.config.js";
 
 const dotenvPath = findConfig(".env");
 dotenv.config({ path: dotenvPath });
 
+connectDB()
+  .then(() => {
+    console.log("Successfully connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
+
 const app = express();
 const port = process.env.SERVER_PORT || 3000;
 
+// Enable CORS
+app.use(cors({ origin: "*" }));
+
+// Parse requests of content-type - application/json
+app.use(express.json());
+
+// base route
 app.get("/", (_, res) => {
-  res.send("Hello World!");
+  res.send({ message: "Welcome to the Web API" });
 });
 
 app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
+  return console.log(`Server is running at http://localhost:${port}`);
 });
--- src/webapi/config/db.config.ts
+++ src/webapi/config/db.config.ts
@@ -1,9 +1,26 @@
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
+const url =
+  DB_USER && DB_PASSWORD && DB_HOST && DB_PORT && DB_NAME
+    ? `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`
+    : "mongodb://127.0.0.1/wallet-db";
 
-export default {
-  url: `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`,
-};
+mongoose.set("strictQuery", false);
+
+export const db = mongoose.connection;
+
+export function disconnectDB() {
+  return mongoose.disconnect();
+}
+
+export function testConnection() {
+  return mongoose.connection.readyState;
+}
+
+export default async function connectDB() {
+  await mongoose.connect(url);
+}
