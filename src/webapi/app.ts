f2c731b41619fe1d0fa1a90cbb13fdcdbafeb6cf
daniacca <d.acca87@gmail.com>
2023-10-31 15:20:00 +0100

    feat: added health check endpoint

--- src/webapi/app.ts
+++ src/webapi/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import findConfig from "find-config";
 import cors from "cors";
-import connectDB from "./config/db.config.js";
+import connectDB, { testConnection } from "./config/db.config.js";
 
 const dotenvPath = findConfig(".env");
 dotenv.config({ path: dotenvPath });
@@ -28,6 +28,15 @@ app.use(express.json());
 // base route
 app.get("/", (_, res) => {
   res.send({ message: "Welcome to the Web API" });
+});
+
+// health check route
+app.get("/health", (_, res) => {
+  if (testConnection() !== 1) {
+    res.status(500).send({ message: "Server is down" });
+    return;
+  }
+  res.send({ message: "Server is up and running" });
 });
 
 app.listen(port, () => {
